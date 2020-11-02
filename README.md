202e7a7f9ef41531694a56015c7ce766ba5a4f29
Daniel Seiler <daniel.seiler@fachschaft.informatik.tu-darmstadt.de>
2020-11-02 14:24:21 +0100

    Initial release

--- README.md
+++ README.md
@@ -2,3 +2,38 @@
 
 A simple django app that uses [gitpython](https://gitpython.readthedocs.io/) to provide
 templatetag(s) to display git version info from the current repo.
+
+## Installation
+
+First, install this package using `pip`
+
+```
+$ pip install django-git-version
+```
+
+then add `git_version` to `INSTALLED_APPS` in your Django `settings.py`:
+
+```python
+INSTALLED_APPS = (
+    ...
+    'git_version'
+    ...
+)
+```
+
+## Usage
+
+Load the templatetag with
+
+```django
+{% load git_tags %}
+```
+
+and then use one of
+
+```django
+{% current_commit_short '.' %}
+{% current_commit '.' %}
+```
+
+Where `'.'` is the path to the git repo
--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 import os
 from setuptools import find_packages, setup
 
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='django-git-version',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
     description='django ',
-    long_description='README.md',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/d120/django-git-version',
-    author='Daniel Schweighöfer',
-    author_email='dschweighoefer@d120.de',
+    author='Daniel Seiler',
+    author_email='daniel.seiler@fachschaft.informatik.tu-darmstadt.de',
     install_requires=[
-        'django',
+        'django<4',
         'gitpython',
     ],
     classifiers=[
