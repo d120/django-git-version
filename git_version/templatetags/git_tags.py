1e5cd2187b4686465d8066e6df00930e2df8e5d4
Russ Ferriday <topiaruss@gmail.com>
2021-11-15 14:53:49 +0000

    Add messages for repo error

--- git_version/templatetags/git_tags.py
+++ git_version/templatetags/git_tags.py
@@ -5,11 +5,15 @@ register = template.Library()
 
 @register.simple_tag
 def current_commit(path):
-    repo = Repo(path)
-    commit = repo.commit()
-    chash = commit.hexsha
-    repo.__del__()
-    return chash
+    try:
+        repo = Repo(path)
+        commit = repo.commit()
+        chash = commit.hexsha
+        repo.__del__()
+        return chash
+    except Exception:
+        print(f"git_version - trouble with repo path {path}")
+        return "no git?"
 
 @register.simple_tag
 def current_commit_short(path):
